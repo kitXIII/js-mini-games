353457cba872c132d735e1c5f27dee6f26f3ac7c
Nikita <mipo4t@gmail.com>
2018-07-09 19:03:06 +0400

    include eslint in project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 const greet = () => {
   const userName = readlineSync.question('May I have your name?\n');
   console.log(`Hello, ${userName}`);
-} 
+};
 
 export default greet;
