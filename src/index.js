ffbea038841bfb40a3c5c71b9961c4d7a7e4495c
Nikita <mipo4t@gmail.com>
2018-07-10 10:09:44 +0400

    add return username form greet function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import readlineSync from 'readline-sync';
 const greet = () => {
   const userName = readlineSync.question('May I have your name?\n');
   console.log(`Hello, ${userName}`);
+  return userName;
 };
 
 export default greet;
